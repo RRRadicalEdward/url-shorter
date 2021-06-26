3a2e79f8b595fc4f8fe0547f811902d8cdb0dcbb
Alex Yusuk <thepulticula@gmail.com>
2021-06-26 17:55:43 +0300

    Implement backend service around saphir HTTP library

---
+++ url-shorter-backend/src/main.rs
@@ -0,0 +1,36 @@
+use dotenv::dotenv;
+use lib::{
+    db::Database,
+    utils,
+    web::{HealthController, ShorterController},
+};
+use saphir::prelude::*;
+use slog::{info, o};
+use std::convert::Into;
+
+const DEFAULT_IP_ADDR: &str = "127.0.0.1:8080";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let logger = utils::build_logger();
+    let web_logger = logger.new(o!("web" => "request"));
+    let db_logger = logger.new(o!("Database" => "connection"));
+
+    dotenv().ok();
+    let database = Database::establish_connection(db_logger)?;
+
+    let server = Server::builder()
+        .configure_listener(|l| l.interface(DEFAULT_IP_ADDR).server_name("url-shorter"))
+        .configure_router(|router| {
+            info!(web_logger, "Running web service on {}", DEFAULT_IP_ADDR);
+
+            router
+                .controller(HealthController {})
+                .controller(ShorterController::new(database, web_logger))
+        })
+        .build();
+
+    server.run().await.map_err(Into::<anyhow::Error>::into)?;
+
+    Ok(())
+}
---
+++ url-shorter-backend/src/web.rs
@@ -0,0 +1,116 @@
+use super::db::{models, Database};
+use crate::utils::{HttpResponseEx, OptionLogger, ResultLogger};
+use saphir::{
+    body::Body,
+    controller::{Controller, ControllerEndpoint, EndpointsBuilder},
+    http::{Method, StatusCode},
+    macros::controller,
+    request::Request,
+    response::{Builder as HttpResponse, Response},
+};
+use slog::{debug, info, o, Logger};
+use url::Url;
+
+pub struct ShorterController {
+    database: Database,
+    web_logger: Logger,
+}
+
+impl ShorterController {
+    pub fn new(database: Database, web_logger: Logger) -> Self {
+        Self { database, web_logger }
+    }
+
+    pub async fn redirect_to_origin_url(&self, req: Request<Body>) -> Result<HttpResponse, HttpResponse> {
+        let shorted_url = req.captures().get("shorted_url").ok_or(()).or_bad_request()?;
+
+        let db_logger = self
+            .database
+            .logger
+            .new(o!("check if shorted url exists in DB" => format!("shorter_url: {}", shorted_url)));
+
+        let web_logger = self
+            .web_logger
+            .new(o!("check if shorted URL exists in DB" => format!("shorter_url: {}", shorted_url)));
+
+        let origin_url = self
+            .database
+            .get_origin_url(shorted_url.clone())
+            .log_on_err(&db_logger, "Failed to check if shorted URL exists in database")
+            .or_bad_request()?
+            .log_on_none(
+                &web_logger,
+                format!("Did not find `{}` in the DB", shorted_url).as_str(),
+            )
+            .ok_or(())
+            .or_bad_request()?;
+
+        debug!(
+            web_logger,
+            "Found `{}` shorted URL  in the DB and its related to `{}` URL", shorted_url, origin_url
+        );
+
+        Ok(Response::<Body>::builder()
+            .status(StatusCode::PERMANENT_REDIRECT)
+            .header("Location", origin_url))
+    }
+
+    pub async fn short_url(&self, mut req: Request) -> Result<HttpResponse, HttpResponse> {
+        let url = req.body_mut().take_as::<String>().await.or_bad_request()?;
+
+        let web_logger = self.web_logger.new(o!("Shortening URL: " => url.clone()));
+
+        Url::parse(url.as_str())
+            .log_on_err(&web_logger, format!("Got incorrect URL: {}", url).as_str())
+            .or_bad_request()?;
+
+        let db_logger = self.database.logger.new(o!("Shortening URL: " => url.clone()));
+
+        let unique_id = self
+            .database
+            .get_new_unique_id()
+            .log_on_err(&db_logger, "Failed to get a unique id")
+            .or_internal_error()?;
+
+        debug!(self.web_logger, "Got a unique_id: {}", unique_id);
+
+        let shorted_url = radix_fmt::radix_36(unique_id).to_string();
+
+        let url_model = models::Url::new(shorted_url.as_str(), url.as_str());
+
+        self.database
+            .save_shorter_url(url_model)
+            .log_on_err(&self.database.logger, "Failed to save shorted URL: {}")
+            .or_internal_error()?;
+
+        info!(
+            self.web_logger,
+            "Shorted `{}` URL to `{}` URL successfully", url, shorted_url
+        );
+
+        Ok(Response::<Body>::builder().status(StatusCode::OK).body(shorted_url))
+    }
+}
+
+impl Controller for ShorterController {
+    const BASE_PATH: &'static str = "/";
+
+    fn handlers(&self) -> Vec<ControllerEndpoint<Self>>
+    where
+        Self: Sized,
+    {
+        EndpointsBuilder::new()
+            .add(Method::GET, "/shorted_url/<shorted_url>", Self::redirect_to_origin_url)
+            .add(Method::POST, "/short_url", Self::short_url)
+            .build()
+    }
+}
+pub struct HealthController {}
+
+#[controller(name = "health")]
+impl HealthController {
+    #[get("/")]
+    pub async fn health_controller(&self) -> (StatusCode, String) {
+        (StatusCode::OK, String::from("It's ALIVE!!!"))
+    }
+}
